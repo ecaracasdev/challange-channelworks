671cec924dac794b6a10f7edb429ad7a4386f522
elias.caracas <elias.caracas@eggcooperation.com>
2023-04-15 10:28:31 -0300

    add set up

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:eslint-comments/recommended',
+    'plugin:jest/recommended',
+    'plugin:promise/recommended',
+    'prettier',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'import', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'import/extensions': 'off',
+    'no-console': 'off',
+  },
+};
